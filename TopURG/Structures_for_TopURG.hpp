0691dae47f1c0df0c0f71b14b94b08481733f8a7
nishida_macbookpro <daiki.nishida@gmail.com>
2013-07-22 20:25:52 +0900

    TopURG2ssmを追加。

---
+++ TopURG/Structures_for_TopURG.hpp
@@ -0,0 +1,5 @@
+typedef struct{
+	double dist[682];	/* 距離データ[m] */
+	double x[682];		/* x座標[m] */
+	double y[682];		/* y座標[m] */
+}TopURG;
---
+++ TopURG/TopURGctrl.h
@@ -0,0 +1,10 @@
+#include "Structures_for_TopURG.hpp"
+
+
+extern void TopURG_Init(char *dev_name);
+extern void TopURG_GetData(TopURG *urg_data);
+
+
+
+
+
