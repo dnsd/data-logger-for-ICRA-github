8fe37287634f2a79e50093fe6e444646dc43cba4
dnsd-S10 <daiki.nishida@gmail.com>
2013-07-23 22:17:10 +0900

    全面書き換え

--- TopURG/TopURG2gnu.cpp
+++ TopURG/TopURG2gnu.cpp
@@ -1,29 +1,68 @@
 #include <iostream>
 #include <cmath>
-#include "GNUctrl_for_TopURG.h"
 #include <ssm.hpp>
+#include <cstdio>
+#include "LRF.h"
 
-using namespace std;
+#define GP_PATH "/usr/bin/gnuplot"
+
+FILE *gnu_p;
+
+void GNU_Init(void)
+{
+	// Gnuplotのオープン
+	gnu_p = popen(GP_PATH, "w");
+	fprintf(gnu_p, "set xrange [-5:5]\n");
+	fprintf(gnu_p, "set xlabel \"x[m]\"\n");
+	fprintf(gnu_p, "set yrange [-5:5]\n");
+	fprintf(gnu_p, "set ylabel \"y[m]\"\n");
+	fprintf(gnu_p, "set grid\n");
+	fprintf(gnu_p, "set size square\n");
+	fprintf(gnu_p, "unset key\n");
+}
+
+void GNU_Plot(LRF *urg_data)
+{
+	int i;
+
+	fprintf(gnu_p, "plot \"-\"\n");	// バッファ出力開始
+
+	// LRFの二次元座標データをバッファに出力
+	for (i = 0; i < 1081; i++)
+	{
+		fprintf(gnu_p, "%lf %lf\n", urg_data->x[i], urg_data->y[i]);
+	}
+
+	// fprintf(gnu_p, "%lf %lf\n", urg_data->x[0], urg_data->y[0]);
+	// fprintf(gnu_p, "%lf %lf\n", urg_data->x[1060], urg_data->y[1060]);
+
+	fprintf(gnu_p, "e\n");	// バッファ出力終了
+	fflush(gnu_p);			// Gnuplotに描画させる
+}
+
+void GNU_Close(void)
+{	
+	fclose(gnu_p);
+}
 
 int main(int aArgc, char **appArgv)
 {
 	initSSM();	//SSMイニシャライズ
 
-	TopURG urg_data;	// TopURGの座標データ
+	LRF urg_data;	// LRFの座標データ
 
 	GNU_Init();	// Gnuplotの初期化
-
+	
 	SSM_sid URG_sid; //SSM管理id
 	double time; //SSM書き込み時間
 
-	URG_sid = openSSM("URG",0,0);	//SSMを開く（”名前”，番号，時間）
+	URG_sid = openSSM("Top-URG",0,0);	//SSMを開く（”名前”，番号，時間）
 
 	while(1)
 	{
 		if(readSSM(URG_sid, (char*)&urg_data, &time,-1)){	//読みにいく（if文はエラー処理）
 			GNU_Plot(&urg_data);	// Gnuplotに描画
-			// usleep(200000); //0.2秒
-			usleep(10000); //0.01秒
+			usleep(200000);
 		}
 	}
 	
---
+++ TopURG/urg2ssm.c
@@ -0,0 +1,192 @@
+/* URGから得られたスキャンデータをSSMに書き込む                  */
+/* Author Suehiro lab @ Department of Inteligent system  */
+/* Refer to Atushi Watanabe                              */
+/* Date 2010 05 10                                       */
+
+/**
+  @file libscip2awd_test.c
+  @brief Test program for libscip2
+  @author Atsushi WATANEBE <atusi_w@roboken.esys.tsukuba.ac.jp> <atusi_w@doramanjyu.com>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <signal.h>
+
+// #include <libscip2awd.h>
+#include <scip2awd.h>
+#include "LRF.h"
+#include <ssm.h>
+#include <math.h>
+
+//#define USE_GS
+
+
+/** Flag */
+int gShutoff;
+
+/**
+  @brief Ctrl+C trap
+  @param aN not used
+ */
+void ctrlc( int aN )
+{
+  /* It is recommended to stop URG outputting data. */
+  /* or cost much time to start communication next time. */
+  gShutoff = 1;
+  signal( SIGINT, NULL );
+}
+
+/*
+   @brief Main function
+   @param aArgc Number of Arguments 
+   @param appArgv Arguments
+   @return failed: 0, succeeded: 1 
+*/
+int main( int aArgc, char **appArgv )
+{
+  /* URG2SSM */
+  LRF SCAN_DATA;
+  initSSM();
+  SSM_sid LRF_sid;
+  double utime;
+  LRF_sid=createSSM_time("Top-URG",0,sizeof(LRF),0.5,0.025);
+  
+  /* Device Port */
+  S2Port *port;
+  /* Data recive dual buffer */
+  S2Sdd_t buf;
+  /* Pointer to data buffer */
+  S2Scan_t *data;
+  /* Loop valiant */
+  int j;
+  /* Returned value */
+  int ret;
+  /* Local time */
+  struct timeval tm;
+
+ /*
+  if( aArgc != 2 ){
+    fprintf( stderr, "USAGE: %s device\n", appArgv[0] );
+    return 0;
+  }
+  */
+
+  /* Start trapping ctrl+c signal */
+  gShutoff = 0;
+  signal( SIGINT, ctrlc );
+  
+  /* Open the port */
+  if(aArgc<2)
+    port = Scip2_Open( "/dev/ttyACM0", B115200 );
+  if(aArgc == 2)
+    port = Scip2_Open( appArgv[1], B115200 );
+  if( port == 0 )
+    {
+      fprintf( stderr, "ERROR: Failed to open device.\n" );
+      return 0;
+    }
+  printf("Port opened\n");
+
+  /* Initialize buffer before getting scanned data */
+  S2Sdd_Init( &buf );
+  printf("Buffer initialized\n");
+
+  /* Demand sending me scanned data */
+  /* Data will be recived in another thread  */
+  /* MS command */
+  Scip2CMD_StartMS( port, 0, 1080, 1, 0, 0, &buf, SCIP2_ENC_3BYTE );
+
+  //SSMのLRF_RANGE_DATA初期化
+  for(j=0;j<1080;j++)
+    {
+      SCAN_DATA.dist[j]=0.0;
+      SCAN_DATA.x[j]=0.0;
+      SCAN_DATA.y[j]=0.0;
+    }
+
+  while( !gShutoff )
+    {
+      /* Start using scanned data */
+      utime=gettimeSSM();
+      ret = S2Sdd_Begin( &buf, &data );
+      /* Returned value is 0 when buffer is having been used now */
+      if( ret > 0 )
+	{
+	  /* You can start reciving next data after S2Sdd_Begin succeeded */
+	  gettimeofday( &tm, NULL );
+	  /* then you can analyze data in parallel with reciving next data */
+	  
+	  /* ---- analyze data ---- */
+	  gettimeofday( &tm, NULL );
+	  
+	  //SSMに書き込む
+	  for( j = 0; j < data->size; j++ )
+	    {
+	      SCAN_DATA.dist[j] = (double)data->data[j]/1000.0;
+	      SCAN_DATA.x[j] = SCAN_DATA.dist[j]*cos((-135+0.25*j)*(M_PI/180));;
+	      SCAN_DATA.y[j] = SCAN_DATA.dist[j]*sin((-135+0.25*j)*(M_PI/180));;
+	    }
+	  writeSSM(LRF_sid, (char*)&SCAN_DATA, utime);
+	  //正面の距離データだけ表示
+	  printf( "Front Dist [m] =  %lf\n", SCAN_DATA.dist[data->size/2]);
+	  usleep(90000);	  
+	  /* Don't forget S2Sdd_End to unlock buffer */
+	  S2Sdd_End( &buf );
+	}
+      else if( ret == -1 )
+	{
+	  fprintf( stderr, "ERROR: Fatal error occurred.\n" );
+	  Scip2_Close( port );
+	  printf("Port closed\n");
+	  /* Open the port */
+	  if(aArgc<2)
+	    port = Scip2_Open( "/dev/ttyACM0", B115200 );
+	  if(aArgc == 2)
+	    port = Scip2_Open( appArgv[1], B115200 );
+	  if( port == 0 )
+	    {
+	      fprintf( stderr, "ERROR: Failed to open device.\n" );
+	      return 0;
+	    }
+	  printf("Port opened\n");
+	  /* Initialize buffer before getting scanned data */
+	  S2Sdd_Init( &buf );
+	  printf("Buffer initialized\n");
+	  
+	  /* Demand sending me scanned data */
+	  /* Data will be recived in another thread  */
+	  /* MS command */
+	  Scip2CMD_StartMS( port, 0, 1080, 1, 0, 0, &buf, SCIP2_ENC_3BYTE );
+	  continue;
+	  //break;
+	}
+      else
+	{
+	  usleep(100);
+	}
+    }
+  printf( "\nStopping\n" );
+  
+  
+  ret = Scip2CMD_StopMS( port, &buf );
+  if( ret == 0 )
+    {
+      fprintf( stderr, "ERROR: StopMS failed.\n" );
+      return 0;
+    }
+
+  printf("Stopped\n");
+  
+  /* Destruct buffer */
+  S2Sdd_Dest( &buf );
+  printf("Buffer destructed\n");
+  
+  /* Close port */
+  Scip2_Close( port );
+  printf("Port closed\n");
+  
+  return 1;
+}
+
