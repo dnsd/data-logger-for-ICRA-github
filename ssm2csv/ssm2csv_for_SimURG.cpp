82bca4181ff3f532b71d8ab9c6fd3111a936410d
dnsd-S10 <daiki.nishida@gmail.com>
2013-07-25 22:29:51 +0900

    ssm2csvフォルダの作成。

---
+++ ssm2csv/ssm2csv_for_SimURG.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <signal.h>
+
+#include "LRF.h"
+#include <ssm.h>
+#include <ssm.hpp>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+using namespace std;
+
+double utime;
+
+//-SSM-//
+SSMApi<SimURG> SCAN_DATA("Sim-URG", 1);
+
+int main( int aArgc, char **appArgv )
+{
+	// SSM
+	initSSM();
+	SCAN_DATA.open(SSM_READ);
+
+	// ファイル操作
+	ofstream ofs;
+	ofs.open("SimURG_DATA.csv");
+
+	while(1)
+	{
+		if (SCAN_DATA.readNew())
+		{
+			ofs << SCAN_DATA.time << "," << SCAN_DATA.data.dist[340] << endl;
+		}
+		usleep(10000); //0.01秒
+	}
+
+	SCAN_DATA.close();
+	endSSM();
+	return 0;
+}
